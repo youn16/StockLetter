7d24d811282d978572a75ee1d6f15e7741994da0
youn16g <seyounlee16@gmail.com>
2021-01-27 21:49:07 +0900

    [Web] API 기본 추가

--- web/app/routes/Apps/ReportGrid/ReportGrid.js
+++ web/app/routes/Apps/ReportGrid/ReportGrid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //Custom
 import { 
     Container,
@@ -7,60 +7,63 @@ import {
     Col
 } from './../../../components';
 import { HeaderMain } from "../../components/HeaderMain";
-import { ProjectsSmHeader } from "../../components/Projects/ProjectsSmHeader";
 import { ReportCard } from "../../components/Report/ReportCard";
 import { Paginations } from "../../components/Paginations";
+import axios from 'axios';
 
-const ReportGrid = () => (
-    <Container>
-        <HeaderMain 
-            title="News"
-            className="mb-5 mt-4"
-        />
-        { /* START Content */}
-        <Row>
-            <Col lg={ 12 }>
-                { /*
-                <ProjectsSmHeader 
-                    subTitle="ReportGrid"
-                    linkList="/apps/gallery-table"
-                    linkGrid="/apps/gallery-grid"
-                />
-                */ }
-                <CardColumns>
-                    <ReportCard />
-                    <ReportCard 
-                        id="2"
+const ReportGrid = () => {
+
+    return (
+        <Container>
+            <HeaderMain 
+                title="News"
+                className="mb-5 mt-4"
+            />
+            { /* START Content */}
+            <Row>
+                <Col lg={ 12 }>
+                    { /*
+                    <ProjectsSmHeader 
+                        subTitle="ReportGrid"
+                        linkList="/apps/gallery-table"
+                        linkGrid="/apps/gallery-grid"
                     />
-                    <ReportCard 
-                        id="3"
-                    />
-                    <ReportCard 
-                        id="4"
-                    />
-                    <ReportCard 
-                        id="5"
-                    />
-                    <ReportCard 
-                        id="6"
-                    />
-                    <ReportCard 
-                        id="7"
-                    />
-                    <ReportCard 
-                        id="8"
-                    />
-                    <ReportCard 
-                        id="9"
-                    />
-                </CardColumns>
-                <div className="d-flex justify-content-center">
-                    <Paginations />
-                </div>
-            </Col>
-        </Row>
-        { /* END Content */}
-    </Container>
-);
+                    */ }
+                    <CardColumns>
+                        <ReportCard />
+                        <ReportCard 
+                            id="2"
+                        />
+                        <ReportCard 
+                            id="3"
+                        />
+                        <ReportCard 
+                            id="4"
+                        />
+                        <ReportCard 
+                            id="5"
+                        />
+                        <ReportCard 
+                            id="6"
+                        />
+                        <ReportCard 
+                            id="7"
+                        />
+                        <ReportCard 
+                            id="8"
+                        />
+                        <ReportCard 
+                            id="9"
+                        />
+                    </CardColumns>
+                    <div className="d-flex justify-content-center">
+                        <Paginations />
+                    </div>
+                </Col>
+            </Row>
+            { /* END Content */}
+        </Container>
+    );
+};
 
 export default ReportGrid;
