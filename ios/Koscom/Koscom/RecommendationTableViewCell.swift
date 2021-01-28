54f54c1fb04a9bee321bb2b488f393aeddd5ccb4
seoungjunOh <tmdwns0907@naver.com>
2021-01-28 21:57:21 +0900

    recommedation completed

---
+++ ios/Koscom/Koscom/RecommendationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecommendationTableViewCell.swift
+//  Koscom
+//
+//  Created by SeoungJun Oh on 2021/01/28.
+//  Copyright © 2021 SeoungJun Oh. All rights reserved.
+//
+
+import UIKit
+
+class RecommendationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
